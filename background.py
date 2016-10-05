0c6949d6089903f532ed63cbb33f52698ebeb37b
pacozaa <pacosarin@gmail.com>
2016-10-05 14:57:28 +0700

    initial moon

--- background.py
+++ background.py
@@ -4,6 +4,11 @@ import math
 from random import randint
 import background
 
+class vec2d(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 indigo = (121, 134, 203)
 sun_red = (191,54,12)
 radius_red = (230,74,25)
@@ -23,6 +28,7 @@ def draw(display,display_width,display_height):
     draw_sky(display,display_width,display_height)
     draw_sun(display,display_width,display_height)
     draw_wave(display,display_width,display_height)
+    draw_moon(display)
 
 def draw_sun(display,display_width,display_height):
     sun_center = (200, 100)
@@ -60,12 +66,12 @@ def draw_wave(display,display_width,display_height):
     wave_gap = 25
     wave_gang = randint(min_wave,max_wave)
 
-    for wave in range(0,wave_gang):
+    for wave in range(0,wave_gang,2):
         vary_base = randint(30,50)
         amp = randint(vary_base,max_amp)
         wave_start -= wave_gap
         random_sea = randint(0,3)
-        for xaxis in range(0,display_width):
+        for xaxis in range(0,display_width,15):
             if wave == 0 or wave == wave_gang-1:
                 wave = pygame.draw.circle(display, sea, (xaxis, (wave_start)+math.floor((amp)*math.sin(math.radians(xaxis)))), rim_tick, 0)
             elif wave == 0 or wave == wave_gang-1:
@@ -107,3 +113,95 @@ def draw_sky(display,display_width,display_height):
 def draw_sand(display,display_width,display_height):
     sand = pygame.draw.rect(display, earth, (0,0,display_width,display_height), 0)
     return sand
+
+def draw_moon(display):
+    width = 30
+    height = 100
+
+    ### Control points that are later used to calculate the curve
+    control_points1 = [vec2d(100,100), vec2d(50,150), vec2d(50,200), vec2d(100,250)]
+    control_points2 = [vec2d(100,100), vec2d(30,150), vec2d(30,220), vec2d(100,250)]
+    ### Draw control points
+    for p in control_points1:
+        pygame.draw.circle(display, amber, (int(p.x), int(p.y)), 4)
+    for p in control_points2:
+        pygame.draw.circle(display, amber, (int(p.x), int(p.y)), 4)
+
+    ### Draw control "lines"
+    pygame.draw.lines(display, indigo, False, [(x.x, x.y) for x in control_points1])
+    pygame.draw.lines(display, indigo, False, [(x.x, x.y) for x in control_points2])
+
+    ### Draw bezier curve
+    b_points1 = compute_bezier_points([(x.x, x.y) for x in control_points1])
+    b_points2 = compute_bezier_points([(x.x, x.y) for x in control_points2])
+    pygame.draw.lines(display, pygame.Color("red"), False, b_points1, 2)
+    pygame.draw.lines(display, pygame.Color("red"), False, b_points2, 2)
+
+
+
+def compute_bezier_points(vertices, numPoints=None):
+    if numPoints is None:
+        numPoints = 30
+    if numPoints < 2 or len(vertices) != 4:
+        return None
+
+    result = []
+
+    b0x = vertices[0][0]
+    b0y = vertices[0][1]
+    b1x = vertices[1][0]
+    b1y = vertices[1][1]
+    b2x = vertices[2][0]
+    b2y = vertices[2][1]
+    b3x = vertices[3][0]
+    b3y = vertices[3][1]
+
+
+
+    # Compute polynomial coefficients from Bezier points
+    ax = -b0x + 3 * b1x + -3 * b2x + b3x
+    ay = -b0y + 3 * b1y + -3 * b2y + b3y
+
+    bx = 3 * b0x + -6 * b1x + 3 * b2x
+    by = 3 * b0y + -6 * b1y + 3 * b2y
+
+    cx = -3 * b0x + 3 * b1x
+    cy = -3 * b0y + 3 * b1y
+
+    dx = b0x
+    dy = b0y
+
+    # Set up the number of steps and step size
+    numSteps = numPoints - 1 # arbitrary choice
+    h = 1.0 / numSteps # compute our step size
+
+    # Compute forward differences from Bezier points and "h"
+    pointX = dx
+    pointY = dy
+
+    firstFDX = ax * (h * h * h) + bx * (h * h) + cx * h
+    firstFDY = ay * (h * h * h) + by * (h * h) + cy * h
+
+
+    secondFDX = 6 * ax * (h * h * h) + 2 * bx * (h * h)
+    secondFDY = 6 * ay * (h * h * h) + 2 * by * (h * h)
+
+    thirdFDX = 6 * ax * (h * h * h)
+    thirdFDY = 6 * ay * (h * h * h)
+
+    # Compute points at each step
+    result.append((int(pointX), int(pointY)))
+
+    for i in range(numSteps):
+        pointX += firstFDX
+        pointY += firstFDY
+
+        firstFDX += secondFDX
+        firstFDY += secondFDY
+
+        secondFDX += thirdFDX
+        secondFDY += thirdFDY
+
+        result.append((int(pointX), int(pointY)))
+
+    return result
